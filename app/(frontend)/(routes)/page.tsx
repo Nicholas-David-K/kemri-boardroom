2566d1b2eed2b4527d58b10842bbff4a04074686
Nicholas David <onedavid.nicholas@gmail.com>
2024-07-08 16:17:35 +0300

    initial commit

--- app/(frontend)/(routes)/page.tsx
+++ app/(frontend)/(routes)/page.tsx
@@ -1,9 +1,5 @@
-import { ArrowRight, Monitor, PhoneIcon, SunIcon, Users, WifiIcon } from 'lucide-react';
-
-import ActionTooltip from '@/components/action-tooltip';
-import { Button } from '@/components/ui/button';
+import BoardroomItem from '@/components/boardroom-item';
 import Filter from '@/components/filter';
-import Image from 'next/image';
 import WelcomeHeader from '@/components/welcome-header';
 import WidthWrapper from '@/components/width-wrapper';
 
@@ -15,6 +11,7 @@ export default function Home() {
                 <div className="grid grid-cols-1 lg:grid-cols-5 gap-4">
                     <div className="md:col-span-4 col-span-4 w-full">
                         <Filter />
+                        <BoardroomItem />
                     </div>
                     <div className="bg-primary-500 h-full w-full p-10 hidden lg:block"></div>
                 </div>
