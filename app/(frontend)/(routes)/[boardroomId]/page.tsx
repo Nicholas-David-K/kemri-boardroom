36281a84437c663875f6df761964c25ad510e76a
levi <levimulama6@gmail.com>
2024-07-17 16:34:22 +0300

    initial commit

--- app/(frontend)/(routes)/[boardroomId]/page.tsx
+++ app/(frontend)/(routes)/[boardroomId]/page.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import BoardroomInfo from "@/components/boardroom-info";
+import Header from "@/components/header";
+import WidthWrapper from "@/components/width-wrapper";
+import { db } from "@/lib/db";
 
 interface BoardroomDetailPage {
-    params: {
-        boardroomId: string;
-    };
+  params: {
+    boardroomId: string;
+  };
 }
 
-const BoardroomDetailPage = ({ params }: BoardroomDetailPage) => {
-    return <div>BoardroomDetailPage {params.boardroomId}</div>;
+const BoardroomDetailPage = async ({ params }: BoardroomDetailPage) => {
+  const boardroom = await db.boardroom.findUnique({
+    where: {
+      id: params.boardroomId,
+    },
+    include: {
+      images: true,
+    },
+  });
+  return (
+    <div>
+      <Header
+        heading="Boardroom Detail Page"
+        subtitle="View Boardroom Details"
+      />
+      <WidthWrapper>
+        <BoardroomInfo data={boardroom} />
+      </WidthWrapper>
+    </div>
+  );
 };
 
 export default BoardroomDetailPage;
---
+++ components/boardroom-info.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Boardroom, Image as ImageType } from "@prisma/client";
+import Image from "next/image";
+
+interface BoardroomInfoProps {
+  data: Boardroom & {
+    images: ImageType[];
+  };
+}
+
+const BoardroomInfo = ({ data }: BoardroomInfoProps) => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3">
+      <div className="">
+        <Image
+          src={data?.images[0]}
+          alt="boardroom-img"
+          height={100}
+          width={100}
+          objectFit="cover"
+        />
+      </div>
+    </div>
+  );
+};
+export default BoardroomInfo;
